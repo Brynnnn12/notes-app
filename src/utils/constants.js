b121c7bfa1414ac783fbe5b0164c3ede3e05a4c2
Bryan <bryankurniaakbarr12@gmail.com>
2025-03-15 23:05:51 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
+
+
+export const BASE_URL = "https://backend-notes-app-production.up.railway.app/";
+
+
